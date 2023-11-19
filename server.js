661c0db4d961945909f54fa2748509ea64da68d9
Mariliis Toon <mariliistoon@gmail.com>
2023-11-19 17:01:37 +0200

    search

--- server.js
+++ server.js
@@ -4,23 +4,54 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+app.use(express.urlencoded({extended:true}));
 
 app.get('/search', (req, res) => {
-    res.render('search',{ movieDetails: '' });
- });
+    res.render('search', { movieDetails: '' });
+});
 
-app.post('/search', (req, res)=>{
-    let movieData = {
-        title: 'Free Guy',
-        year: 2021,
-        genres: 'Comedy, Action',
-        overview: 'Let there be some text',
-        posterUrl: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xmbU4JTUm8rsdtn7Y3Fcm30GpeT.jpg'
-    };
+app.post('/search', (req, res) => {
+    let userMovieTitle = req.body.movieTitle;
+console.log(userMovieTitle);
 
-    res.render('search', { movieDetails: movieData });
+    let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=4ca94f8b470d7e34bd3f59c3914295c8&query=${userMovieTitle}`;
+    let genresUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=4ca94f8b470d7e34bd3f59c3914295c8&language=en-US';
 
+    let endpoints = [movieUrl, genresUrl];
+
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+        .then(axios.spread((movie, genres) => {
+            const [movieRaw] = movie.data.results;
+            let movieGenreIds = movieRaw.genre_ids;
+            let movieGenres = genres.data.genres;
+
+            let movieGenresArray = [];
+
+            for (let i = 0; i < movieGenreIds.length; i++) {
+                for (let j = 0; j < movieGenres.length; j++) {
+                    if (movieGenreIds[i] === movieGenres[j].id) {
+                        movieGenresArray.push(movieGenres[j].name);
+                    }
+                }
+            }
+
+            let genresToDisplay = '';
+            movieGenresArray.forEach(genre => {
+                genresToDisplay = genresToDisplay + `${genre}, `;
+            });
+            genresToDisplay = genresToDisplay.slice(0, -2) + '.';
+
+            let movieData = {
+                title: movieRaw.title,
+                year: new Date(movieRaw.release_date).getFullYear(),
+                genres: genresToDisplay,
+                overview: movieRaw.overview,
+                posterUrl: `https://image.tmdb.org/t/p/w500${movieRaw.poster_path}`
+            };
+
+            res.render('search', { movieDetails: movieData });
+
+        }));
 });
 
 app.listen(3000, () => {
